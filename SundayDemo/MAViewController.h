74065088fd5476433c01ccfa2cd285ee74323ada
CHLMA123 <machunlei@bogon.dhcp>
2017-07-09 22:44:28 +0800

    如果想要在之后新建的所有类都加上前缀，就需要在Class Prefix处加上自己的前缀：
    如果想要在之后新建的所有类都加上前缀，就需要在Class Prefix处加上自己的前缀：

---
+++ SundayDemo/MAViewController.h
@@ -0,0 +1,13 @@
+//
+//  MAViewController.h
+//  SundayDemo
+//
+//  Created by CHLMA2015 on 2017/7/9.
+//  Copyright © 2017年 MACHUNLEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAViewController : UIViewController
+
+@end
